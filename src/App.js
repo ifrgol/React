c72eebade85f0342f8f476246a70fc12f6d08b78
ifrgol <fargol.fattahi79@gmail.com>
2022-12-25 21:14:40 +0330

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [image,setImg]=useState([])
+  useEffect(()=>{
+    const fetchAPI= async () =>{
+      await axios.get('https://random-d.uk/api/random')
+      .then (response => {
+         setImg(response.data)
+     })} 
+
+    fetchAPI();
+},[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <img src={image.url} alt="d.uk" />
+
     </div>
   );
 }
